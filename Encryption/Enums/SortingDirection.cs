e06e6ec7d8ace38da6f0c0466de6d851d9109a58
veles83 <veles83@gmail.com>
2018-07-12 22:47:00 +0300

    Initial commit

---
+++ Encryption/Enums/SortingDirection.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebUtils.Encryption.Enums
+{
+    public enum SortingDirection
+    {
+        ASC,
+        DESC,
+        UNKNOWN
+    }
+}
---
+++ Encryption/Factory/EncrypterFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebUtils.Encryption.Enums;
+using WebUtils.Encryption.Impl;
+
+namespace WebUtils.Encryption.Factory
+{
+    public class EncrypterFactory
+    {
+        public static IEncrypter CreateEncrypter(EncrypterType eType)
+        {
+            if (eType == EncrypterType.MD5)
+                return new MD5Encrypter();
+            else
+                return null;
+        }
+    }
+}
